f52d5a29b8c5f4038219adf609a52374b908a964
ramirezr69 <ronramirez@patricks-imac.attlocal.net>
2016-11-20 01:19:55 -0600

    Setup Firebase Integration.

---
+++ Social-Network/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social-Network
+//
+//  Created by Ron Ramirez on 11/20/16.
+//  Copyright © 2016 Mochi Apps. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
