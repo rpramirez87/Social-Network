dd2f5f39088e29307b16170694f30f207e15335d
ramirezr69 <ronramirez@patricks-imac.attlocal.net>
2016-11-25 23:42:27 -0600

    Finish Loading TableView cells with Parsed data from Firebase.

--- Social-Network/PostCell.swift
+++ Social-Network/PostCell.swift
@@ -14,9 +14,30 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var captionTextView: UITextView!
+    @IBOutlet weak var likesLabel: UILabel!
+    var post : Post!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func configureCell(post : Post) {
+        self.captionTextView.text = post.caption
+        self.likesLabel.text =  "\(post.likes)"
+        
+        let photoURL = post.imageUrl
+        print(photoURL)
+        let url = NSURL(string : photoURL)
+        
+        //If data can be converted
+        if let data = NSData(contentsOf: url as! URL){
+            //Use the Image
+            let img = UIImage(data : data as Data)
+            self.postImageView.image = img
+        }
+
+        
     }
 
 
